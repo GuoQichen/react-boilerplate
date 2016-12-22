338372b0ca42534f11df356368bfd0769adbe51b
guoqichen <250915274@qq.com>
2016-12-22 21:19:17 +0800

    增加antd按需加载

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { DatePicker } from 'antd'
-import 'antd/dist/antd.less'
 
 class Hello extends React.Component {
     constructor(props) {
